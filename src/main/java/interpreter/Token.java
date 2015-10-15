c3d3cca038db18577619431bf056ccdce89332c2
Isaac Elliott <isaace71295@gmail.com>
2015-10-15 16:07:20 +1000

    Added toString method to all tokens

--- src/main/java/interpreter/Token.java
+++ src/main/java/interpreter/Token.java
@@ -12,12 +12,19 @@ public abstract class Token {
 
     public abstract Type getType();
 
+    @Override
+    public abstract String toString();
+
     public static abstract class Operator extends Token {
         public abstract int eval(int left, int right);
 
         public static class Add extends Token.Operator {
             public Type getType() {
                 return Type.ADD;
+            }
+
+            public String toString() {
+                return "ADD";
             }
 
             public int eval(int left, int right) {
@@ -30,6 +37,10 @@ public abstract class Token {
                 return Type.MOD;
             }
 
+            public String toString() {
+                return "MOD";
+            }
+
             public int eval(int left, int right) {
                 return left % right;
             }
@@ -38,6 +49,10 @@ public abstract class Token {
         public static class Max extends Token.Operator {
             public Type getType() {
                 return Type.MAX;
+            }
+
+            public String toString() {
+                return "MAX";
             }
 
             public int eval(int left, int right) {
@@ -60,17 +75,29 @@ public abstract class Token {
         public Type getType() {
             return Type.VALUE;
         }
+
+        public String toString() {
+            return "VALUE";
+        }
     }
 
     public static class OpenParen extends Token {
         public Type getType() {
             return Type.OPEN_PAREN;
         }
+
+        public String toString() {
+            return "OPEN_PAREN";
+        }
     }
 
     public static class CloseParen extends Token {
         public Type getType() {
             return Type.CLOSE_PAREN;
+        }
+
+        public String toString() {
+            return "CLOSE_PAREN";
         }
     }
 }
