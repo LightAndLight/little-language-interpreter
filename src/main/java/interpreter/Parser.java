2d8caca26e22b75690ffbcc00b99588294fcd3a4
Isaac Elliott <isaace71295@gmail.com>
2015-10-15 17:54:02 +1000

    Better error handling

--- src/main/java/interpreter/Parser.java
+++ src/main/java/interpreter/Parser.java
@@ -11,50 +11,63 @@ public class Parser {
     // 4.	T  -> (A) | number
 
     public SyntaxTree parseString(LinkedList<Token> input) throws InterpreterException {
-        Optional<SyntaxTree> maybeLeftSide = parseLeftSide(input);
-        if (maybeLeftSide.isPresent()) {
-            return maybeLeftSide.get();
-        } else {
-            throw new InterpreterException();
+        SyntaxTree leftSide = parseLeftSide(input);
+        if (!parseToken(input, Token.Type.EOF)) {
+            throw new InterpreterException.UnexpectedTokenException(
+                    input.getFirst().getType(),
+                    Token.Type.EOF);
         }
+        return leftSide;
     }
 
-    private boolean parseToken(LinkedList<Token> input, Token.Type type) {
-        if (input.getFirst().getType() == type) {
+    private boolean parseToken(LinkedList<Token> input, Token.Type type) throws InterpreterException.UnexpectedEndOfInputException {
+        if (type != Token.Type.EOF && input.getFirst().getType() == Token.Type.EOF) {
+            throw new InterpreterException.UnexpectedEndOfInputException();
+        } else if (input.getFirst().getType() != type) {
+            return false;
+        } else {
             input.removeFirst();
             return true;
-        } else {
-            return false;
         }
     }
 
-    private Optional<SyntaxTree> parseValue(LinkedList<Token> input) {
+    private SyntaxTree parseValue(LinkedList<Token> input) throws InterpreterException.UnexpectedTokenException, InterpreterException.UnexpectedEndOfInputException {
         Token token = input.getFirst();
         if (parseToken(input, Token.Type.VALUE)) {
-            return Optional.of(new SyntaxTree.Leaf((Token.Value) token));
+            return new SyntaxTree.Leaf((Token.Value) token);
         } else {
-            return Optional.empty();
+            throw new InterpreterException.UnexpectedTokenException(
+                    token.getType(),
+                    Token.Type.VALUE
+            );
         }
     }
 
-    private Optional<SyntaxTree> parseLeftSide(LinkedList<Token> input) {
-        return parseTerminal(input).map(
-                terminal -> parseRightSide(input).map(
-                        tree -> tree.setLeft(terminal)
-                ).orElse(terminal)
-        );
+    private SyntaxTree parseLeftSide(LinkedList<Token> input) throws InterpreterException.UnexpectedTokenException, InterpreterException.UnexpectedEndOfInputException {
+        SyntaxTree terminal = parseTerminal(input);
+        Optional<SyntaxTree.Branch> maybeRightSide = parseRightSide(input);
+
+        if (maybeRightSide.isPresent()) {
+            return maybeRightSide.get().setLeft(terminal);
+        }
+
+        return terminal;
     }
 
-    private Optional<SyntaxTree> parseTerminal(LinkedList<Token> input) {
+    private SyntaxTree parseTerminal(LinkedList<Token> input) throws InterpreterException.UnexpectedTokenException, InterpreterException.UnexpectedEndOfInputException {
         Optional<SyntaxTree> maybeBracketed = parseBracketed(input);
         if (maybeBracketed.isPresent()) {
-            return maybeBracketed;
+            return maybeBracketed.get();
         } else {
             return parseValue(input);
         }
     }
 
-    private Optional<SyntaxTree.Branch> parseRightSide(LinkedList<Token> input) {
+    private Optional<SyntaxTree.Branch> parseRightSide(LinkedList<Token> input) throws InterpreterException.UnexpectedTokenException, InterpreterException.UnexpectedEndOfInputException {
+        if (input.getFirst().getType() == Token.Type.EOF) {
+            return Optional.empty();
+        }
+
         Optional<SyntaxTree.Branch> maybeMax = parseMax(input);
         if (maybeMax.isPresent()) {
             return maybeMax;
@@ -68,36 +81,47 @@ public class Parser {
         return parseAdd(input);
     }
 
-    private Optional<SyntaxTree> parseBracketed(LinkedList<Token> input) {
-        if (parseToken(input, Token.Type.OPEN_PAREN)) {
-            Optional<SyntaxTree> maybeLeftSide = parseLeftSide(input);
+    private Optional<SyntaxTree> parseBracketed(LinkedList<Token> input) throws InterpreterException.UnexpectedTokenException, InterpreterException.UnexpectedEndOfInputException {
+        if (input.size() > 1 && parseToken(input, Token.Type.OPEN_PAREN)) {
+            SyntaxTree maybeLeftSide = parseLeftSide(input);
             if (parseToken(input, Token.Type.CLOSE_PAREN)) {
-                return maybeLeftSide;
+                return Optional.of(maybeLeftSide);
+            } else {
+                throw new InterpreterException.UnexpectedTokenException(
+                        input.getFirst().getType(),
+                        Token.Type.CLOSE_PAREN
+                );
             }
         }
 
         return Optional.empty();
     }
 
-    private Optional<SyntaxTree.Branch> parseMax(LinkedList<Token> input) {
-        if (input.size() >= 2 && parseToken(input, Token.Type.MAX)) {
-            return parseTerminal(input).map(terminal -> new SyntaxTree.Branch.Max(null, terminal));
+    private Optional<SyntaxTree.Branch> parseMax(LinkedList<Token> input) throws InterpreterException.UnexpectedTokenException, InterpreterException.UnexpectedEndOfInputException {
+        if (input.size() >= 1 && parseToken(input, Token.Type.MAX)) {
+            return Optional.of(
+                    new SyntaxTree.Branch.Max(null, parseTerminal(input))
+            );
         } else {
             return Optional.empty();
         }
     }
 
-    private Optional<SyntaxTree.Branch> parseMod(LinkedList<Token> input) {
-        if (input.size() >= 2 && parseToken(input, Token.Type.MOD)) {
-            return parseTerminal(input).map(terminal -> new SyntaxTree.Branch.Mod(null, terminal));
+    private Optional<SyntaxTree.Branch> parseMod(LinkedList<Token> input) throws InterpreterException.UnexpectedTokenException, InterpreterException.UnexpectedEndOfInputException {
+        if (input.size() >= 1 && parseToken(input, Token.Type.MOD)) {
+            return Optional.of(
+                    new SyntaxTree.Branch.Mod(null, parseTerminal(input))
+            );
         } else {
             return Optional.empty();
         }
     }
 
-    private Optional<SyntaxTree.Branch> parseAdd(LinkedList<Token> input) {
-        if (input.size() >= 2 && parseToken(input, Token.Type.ADD)) {
-            return parseTerminal(input).map(terminal -> new SyntaxTree.Branch.Add(null, terminal));
+    private Optional<SyntaxTree.Branch> parseAdd(LinkedList<Token> input) throws InterpreterException.UnexpectedTokenException, InterpreterException.UnexpectedEndOfInputException {
+        if (input.size() >= 1 && parseToken(input, Token.Type.ADD)) {
+            return Optional.of(
+                    new SyntaxTree.Branch.Add(null, parseTerminal(input))
+            );
         } else {
             return Optional.empty();
         }
