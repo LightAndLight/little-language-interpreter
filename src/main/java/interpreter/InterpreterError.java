73cd61e9fef75d229d3674ec4cb310407c1f9c9c
Isaac Elliott <isaace71295@gmail.com>
2015-10-15 16:08:22 +1000

    Added classes for InterpreterError

--- src/main/java/interpreter/InterpreterError.java
+++ src/main/java/interpreter/InterpreterError.java
@@ -1,4 +1,41 @@
 package interpreter;
 
-public class InterpreterError extends Exception {
+public abstract class InterpreterError {
+    public static class UnexpectedInputError extends InterpreterError {
+        private String input;
+
+        public UnexpectedInputError(String input) {
+            this.input = input;
+        }
+
+        @Override
+        public String toString() {
+            return "Unxpected input: " + input;
+        }
+    }
+
+    public static class UnexpectedTokenError extends InterpreterError {
+        private Token actual;
+        private Token expected;
+
+        public UnexpectedTokenError(Token actual, Token expected) {
+            this.actual = actual;
+            this.expected = expected;
+        }
+
+        @Override
+        public String toString() {
+            return "Unexpected token: "
+                    + actual.toString()
+                    + ", expected: "
+                    + expected.toString();
+        }
+    }
+
+    public static class DivideByZeroError extends InterpreterError {
+        @Override
+        public String toString() {
+            return "Division by zero";
+        }
+    }
 }
