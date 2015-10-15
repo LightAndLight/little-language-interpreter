75cb1c73971edf66a6159276f1eb3f5aaa614eaa
Isaac Elliott <isaace71295@gmail.com>
2015-10-15 15:24:11 +1000

    Started working on a REPL

---
+++ src/main/java/interpreter/Interpreter.java
@@ -0,0 +1,28 @@
+package interpreter;
+
+import java.util.Scanner;
+
+public class Interpreter {
+    public static void main(String[] args) throws InterpreterException {
+        Lexer lexer = new Lexer();
+        Parser parser = new Parser();
+
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+        while (true) {
+            System.out.print(">> ");
+
+            input = scanner.nextLine();
+            if (input.equals(":q")) {
+                break;
+            }
+
+            try {
+                SyntaxTree tree = parser.parseString(lexer.tokenize(input));
+                System.out.println(tree.eval());
+            } catch (InterpreterException e) {
+                System.out.println("An error occurred");
+            }
+        }
+    }
+}
